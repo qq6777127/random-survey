fc639a3bcad6a41800c167ad0d01070baeafe0e3
qq6777127 <blue890804@gmail.com>
2024-12-10 01:04:33 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,7 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://www.surveycake.com/s/ZgWlG',
+	'https://www.surveycake.com/s/rgObB',
+	'https://www.surveycake.com/s/KyqpA',
 ];
